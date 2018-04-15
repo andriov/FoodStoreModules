c76681f49ba80fee86424f309db7c4a9adace75d
andriov <andriov@hotmail.com>
2018-04-14 20:34:49 -0500

    Proof the use of "jitpack" in order to use our modules since internet

--- app/src/main/java/com/example/andresito/foodstoremodules/MainActivity.java
+++ app/src/main/java/com/example/andresito/foodstoremodules/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.andresito.foodstoremodules;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.foodstoreexception.Exceptions;
+
 public class MainActivity extends AppCompatActivity {
+
+    Exceptions exception_prove = new Exceptions();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
